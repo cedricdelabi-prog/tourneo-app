402fc5d199498fde814b77bab7f16f9448f158e6
Cedric DELABI <cedrico62@hotmail.fr>
2026-08-02 15:58:51 +0200

    Correction definitive du QR Code

--- app/tournoi/[id]/page.tsx
+++ app/tournoi/[id]/page.tsx
@@ -30,7 +30,6 @@ export default function TournoiPage() {
 
   useEffect(() => {
     setLienPartage(`${window.location.origin}/partage/${params.id}`);
-
     async function initialiser() {
       const {
         data: { user },
